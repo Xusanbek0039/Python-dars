b46b4fff6e9e2c8cba8385451dccf318db25eced
Snouden0071 <itcreative0071@gmail.com>
2023-11-27 03:19:49 +0500

    15- dars yuklandi

---
+++ Dars_15/Darslik/Dars_16_kodlar_jamlanmasi.py
@@ -0,0 +1,69 @@
+# kod_1
+# ismlar = []
+
+# print("Yaqin do'stlaringiz ro'yxatini tuzamiz.")
+# n=1 # ismlarni sanash uchun o'zgaruvchi
+# while True:
+#     savol = f"{n}-do'stingiz ismini kiriting:"
+#     ism = input(savol)
+#     ismlar.append(ism)
+#     javob = input("Yana ism qo'shasizmi? (ha/yo'q)")
+#     if javob =='ha':
+#         n+=1
+#         continue
+#     else:
+#         break
+
+# print("Do'stlaringiz ro'yxati:")
+# for ism in ismlar:
+#     print(ism.title())
+
+
+
+
+
+# kod_2
+
+
+# print("Do'stlaringiz yoshini saqlaymiz.")
+# dostlar = {}
+# ishora = True
+# while ishora:
+#     ism = input("Do'stingiz ismini kiriting: ")
+#     yosh = input(f"{ism.title()}ning yoshini kiriting: ")
+#     dostlar[ism] = int(yosh) # ism kalit, yosh qiymat
+    
+#     javob = input("Yana ma'lumot qo'shasizmi? (ha/yo'q)")
+#     if javob == "yo'q":
+#         ishora = False
+
+# for ism, yosh in dostlar.items():
+#     print(f"{ism.title()} {yosh} yoshda")
+
+
+
+
+
+
+# kod_3
+# cars = ['lacetti','nexia','toyota','nexia','audi','malibu','nexia']
+# while 'nexia' in cars: # toki nexia cars ro'yxati ichida ekan...
+#     cars.remove('nexia') # nexia ni ro'yxatdan olib tashla
+# print(cars)
+
+
+
+
+
+
+
+
+# kod_4
+
+# talabalar = ['hasan', 'husan', 'olim', 'botir']
+# baholangan_talabalar = {}
+# while talabalar:
+#     talaba = talabalar.pop()
+#     baho = input(f"{talaba.title()}ning bahosini kiriting: ")
+#     print(f"{talaba.title()} baholandi")
+#     baholangan_talabalar[talaba] = baho
