f2f0d1502adc7333d492fbcd3609a38a0ab86b1c
Snouden0071 <itcreative0071@gmail.com>
2023-11-29 14:14:26 +0500

    16

---
+++ Dars_16/Darslik/16-dars_kodlar jamlanmasi.py
@@ -0,0 +1,164 @@
+# kod_1
+# def salom_ber():
+#     """Salom beruvchi funksiya"""
+#     print("Assalomu alaykum!")
+
+# salom_ber()
+
+
+
+
+
+
+# kod_2
+# def salom_ber(ism):
+#     """Fodyalanuvchi ismini qabul qilib, unga salom beruvchi funksiya"""
+#     print(f"Assalomu alaykum, hurmatli {ism.title()}!")
+# salom_ber('Xusan')
+
+
+
+
+# kod_3
+# def salom_ber(xusan):
+#     """Fodyalanuvchi ismini qabul qilib, 
+#         unga salom beruvchi funksiya"""
+#     print(f"Assalomu alaykum, hurmatli {xusan.title()}!")
+
+
+# salom_ber('xusan')
+
+
+
+
+
+
+
+
+# Kod_4
+# def salom_ber(xusan):
+#     """Fodyalanuvchi ismini qabul qilib, 
+#         unga salom beruvchi funksiya"""
+#     print(f"Assalomu alaykum, hurmatli {xusan.title()}!")
+
+
+# print(salom_ber.__doc__)
+
+
+
+
+
+
+# kod_5
+# def salom_ber(ism):
+#     """Fodyalanuvchi ismini qabul qilib, unga salom beruvchi funksiya"""
+#     print(f"Assalomu alaykum, hurmatli {ism.title()}!")
+# salom_ber('Xusan')
+# salom_ber('hasan')
+# salom_ber('olim')
+
+
+
+
+# kod_6
+# def toliq_ism(ism, familiya):
+#     """Foydalanuvchi ism va familiyasini jamlab chiqaruvchi funksiya"""
+#     print(f"Foydalanuvchi ismi: {ism.title()}\n"
+#           f"Foydalanuvchi familiyasi: {familiya.title()}")
+    
+
+# toliq_ism('olim','hakimov')
+
+
+
+
+
+# kod_7
+# def toliq_ism(ism, familiya):
+#     """Foydalanuvchi ism va familiyasini jamlab chiqaruvchi funksiya"""
+#     print(f"Foydalanuvchi ismi: {ism.title()}\n"
+#           f"Foydalanuvchi familiyasi: {familiya.title()}")
+    
+# toliq_ism('hakimov','olim')
+
+
+
+
+# Kod_8
+# def yosh_hisobla(ism, tugilgan_yil):
+#     """Foydalanuvchi yoshini hisoblaydigan dastur"""
+#     print(f"{ism.title()} {2020-tugilgan_yil} yoshda")
+
+# # yosh_hisobla('olim', 1997)
+# yosh_hisobla(1997, 'olim')
+
+
+
+
+
+# kod_9
+# def yosh_hisobla(ism, tugilgan_yil):
+#     """Foydalanuvchi yoshini hisoblaydigan dastur"""
+#     print(f"{ism.title()} {2020-tugilgan_yil} yoshda")
+
+# yosh_hisobla(tugilgan_yil=1997, ism='olim')
+
+
+
+
+# kod_10
+# def toliq_ism(ism, familiya):
+#     """Foydalanuvchi ism va familiyasini jamlab chiqaruvchi funksiya"""
+#     print(f"Foydalanuvchi ismi: {ism.title()}\n"
+#           f"Foydalanuvchi familiyasi: {familiya.title()}")
+    
+
+# toliq_ism(familiya='hakimov',ism='olim')
+
+
+
+
+# Kod_11
+# def yosh_hisobla(tugilgan_yil, joriy_yil=2023): # joriy yil uchun st.qiymat 2023
+#     """Foydalanuvchi tug'ilgan yilidan uning yoshini hisoblaydi"""
+#     print(f"Siz {joriy_yil-tugilgan_yil} yoshdasiz")
+
+# # yosh_hisobla(2005,2023)
+# yosh_hisobla(2005)
+
+
+
+
+
+
+
+# Kod_12
+# def yosh_hisobla(tugilgan_yil, joriy_yil=2020):
+#     """Foydalanuvchi tug'ilgan yilidan uning yoshini hisoblaydi"""
+#     print(f"Siz {joriy_yil-tugilgan_yil} yoshdasiz")
+    
+# tyil = input("Tug'ilgan yilingizni kiriting: ")
+# yosh_hisobla(tyil)
+
+
+
+
+
+
+
+# Kod_13
+# def yosh_hisobla(tugilgan_yil, joriy_yil):
+#     """Foydalanuvchi tug'ilgan yilidan uning yoshini hisoblaydi"""
+#     print(f"Siz {joriy_yil-tugilgan_yil} yoshdasiz")
+
+# yosh_hisobla(2005)
+
+
+
+
+# Kod_14
+# def salom_ber():
+#     """Salom beruvchi funksiya"""
+#     print("Assalomu alaykum!")
+
+# salom_ber('hasan')
