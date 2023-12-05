277b924b48eeca66a9823c7ef0d9f68a1d37742a
Xusanbek Suyunov <109731657+Xusanbek0071@users.noreply.github.com>
2023-12-05 19:32:16 +0500

    19

---
+++ Dars_19/Darslik/19-dars-kodlar-jamlanmasi.py
@@ -0,0 +1,164 @@
+# Kod_1
+# class MyClass:
+#   x = 5
+
+# p1 = MyClass()
+# print(p1.x)
+
+
+
+
+
+
+
+
+
+
+# Kod_2
+# class Person:
+#   def __init__(self, name, age):
+#     self.name = name
+#     self.age = age
+
+# p1 = Person("Xusan", 18)
+
+# print(p1.name)
+# print(p1.age)
+
+
+
+
+# Kod_3
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+# person = Person("Furqat", 25)
+# print(person.name)
+# print(person.age)
+
+
+
+
+
+
+
+# Kod_4
+# class Person:
+#   def __init__(self, name, age):
+#     self.name = name
+#     self.age = age
+
+# p1 = Person("John", 36)
+
+# print(p1)
+
+
+
+
+
+
+
+
+
+# # kod_5
+# class Person:
+#   def __init__(self, name, age):
+#     self.name = name
+#     self.age = age
+
+#   def __str__(self):
+#     return f"{self.name}({self.age})"
+
+# p1 = Person("John", 36)
+
+# print(p1)
+
+
+
+
+
+
+
+
+
+
+
+
+# Kod_6
+# class Person:
+#   def __init__(self, name, age):
+#     self.name = name
+#     self.age = age
+
+#   def myfunc(self):
+#     print("Salom mening ismim  " + self.name)
+#     print("Yoshim esa "+ self.age)
+
+# p1 = Person("Xusan", '18')
+# p1.myfunc()
+
+
+
+# # Kod_7
+# class Person:
+#   def __init__(self, name, age):
+#     self.name = name
+#     self.age = age
+
+#   def myfunc(self):
+#     print("Hello my name is " + self.name)
+
+# p1 = Person("John", 36)
+
+# p1.age = 40
+
+# print(p1.age)
+
+
+
+
+
+
+
+
+
+
+# kod_8
+# class Person:
+#   def __init__(self, name, age):
+#     self.name = name
+#     self.age = age
+
+#   def myfunc(self):
+#     print("Hello my name is " + self.name)
+
+# p1 = Person("Xusan",18)
+
+# del p1.age
+
+# print(p1.name)
+
+
+# Taxlil uchun kod 
+# class Animal:
+#     def __init__ (self, name, age):
+#         self.name = name
+#         self.age = age
+
+#     def speak(self):
+#         print("Men kuchli hayvonman!")
+
+#     def introduce(self):
+#         print(f"Mening ismim {self.name}. {self.age} yoshdayman.")
+# lion = Animal("Sher", 5)
+# lion.speak()  # "Men kuchli hayvonman!"
+# lion.introduce()  # "Mening ismim Sher. 5 yoshdayman."
+
+
+
+
+
+
+
---
+++ Dars_19/son-top/main.py
@@ -0,0 +1,5 @@
+
+import funksiyalar as f
+
+print("Keling o'ylagan sonni topish o'ynaymiz!")
+f.play(10)
