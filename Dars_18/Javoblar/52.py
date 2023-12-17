1d17142aa8e2f256a9d87174ebecd1ada61d95d0
Xusanbek Suyunov <109731657+Xusanbek0071@users.noreply.github.com>
2023-12-17 21:52:27 +0500

    Create 52.py

---
+++ Dars_18/Javoblar/52.py
@@ -0,0 +1,11 @@
+def fibonacci(n):
+    sonlar = []
+    for x in range(n):
+        if x == 0 or x == 1:
+            sonlar.append(1)
+        else:
+            sonlar.append(sonlar[x - 1] + sonlar[x - 2])
+    return sonlar
+
+
+print(fibonacci(10))
