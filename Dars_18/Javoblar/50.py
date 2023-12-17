b5b9a68320d843c90617838048271dd4c61cb46a
Xusanbek Suyunov <109731657+Xusanbek0071@users.noreply.github.com>
2023-12-17 21:51:32 +0500

    Create 50.py

---
+++ Dars_18/Javoblar/50.py
@@ -0,0 +1,36 @@
+
+def mijoz_info(ism, familiya, tyil, tjoy, email="", tel=None):
+    """Mijoz haqidagi ma'lumotlarni lug'at ko'rinishida qaytaruvchi funksiya"""
+    mijoz = {
+        "ism": ism,
+        "familiya": familiya,
+        "tyil": tyil,
+        "yoshi": 2020 - tyil,
+        "tjoy": tjoy,
+        "email": email,
+        "telefon": tel,
+    }
+    return mijoz
+
+
+print("Mijoz haqida ma'lumotlarni kiriting.")
+mijozlar = []
+while True:
+    ism = input("Ismi: ")
+    familiya = input("Familiyasi: ")
+    tyil = int(input("Tug'ilgan yili: "))
+    tjoy = input("Tug'ilgan joyi: ")
+    email = input("Email: ")
+    telefon = input("Telefon raqami: ")
+    mijozlar.append(mijoz_info(ism, familiya, tyil, tjoy, email, telefon))
+    javob = input("Davom etasizmi? (ha/yo'q)")
+    if javob != "ha":
+        break
+
+print("Mijozlar:")
+for mijoz in mijozlar:
+    print(
+        f"{mijoz['ism'].title()} {mijoz['familiya'].title()},"
+        f"{mijoz['yoshi']} yoshda, telefoni: +998{mijoz['telefon']}"
+        f"{mijoz['email']}@gmail.com"
+    )
