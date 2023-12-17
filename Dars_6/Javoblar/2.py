69644dc979ad16ebd6b306ef80c1cf4b25f8bd95
Xusanbek Suyunov <109731657+Xusanbek0071@users.noreply.github.com>
2023-12-17 21:49:20 +0500

    Create 2.py

---
+++ Dars_6/Javoblar/2.py
@@ -0,0 +1,11 @@
+countries = ["O'zbekiston", "Rossiya", "Xitoy", "Hindiston", "AQSH"]
+
+print(countries)
+
+print(f"\nRo'yxat uzunligi: {len(countries)}")
+
+print("\nTartiblangan ro'yxat:")
+print(sorted(countries))
+
+print("\nTeskari tartibdagi ro'yxat:")  
+print(sorted(countries, reverse=True))
