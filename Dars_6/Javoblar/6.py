4e947d1a1e01a1424472a8eb40366beca510ed5b
Xusanbek Suyunov <109731657+Xusanbek0071@users.noreply.github.com>
2023-12-17 21:48:58 +0500

    Create 6.py

---
+++ Dars_6/Javoblar/6.py
@@ -0,0 +1,15 @@
+"""
+my_familiy degan ozgaruvchi olamiz ozgaruvchini list
+korinishida oila azolarimiz ismi bilan to'ldiramiz.
+Keyingi navbatda oila azolarimiz ismini konsolga chiqaramiz.
+Nechta oila azolarimiz bor ekaniligi len metodidan faydalangan halda sanab konsolga chiqaramiz.
+sorted metodan foydalangan holda uni tartiblaymiz va reverse funksiyasidan foydalanib teskari tartibda konsolga chiqaramiz."""
+
+My_familiy=["Doniyor","Eldor","Shohrux","Karim"]
+print(My_familiy[0])
+print(My_familiy[1])
+print(My_familiy[2])
+print(My_familiy[3])
+print(len(My_familiy))
+print(sorted(My_familiy,reverse=True))
+# print (My_familiy.reverse(==False))
