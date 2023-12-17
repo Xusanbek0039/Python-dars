cb8a98769f8aabdaca5b3466ae21c1901f11d053
Xusanbek Suyunov <109731657+Xusanbek0071@users.noreply.github.com>
2023-12-17 21:47:11 +0500

    Create 1.py

---
+++ Dars_5/Javoblar/1.py
@@ -0,0 +1,5 @@
+friends = ["Ali", "Mahmud", "Shuhrat"]
+
+print(f"Salom {friends[0]} ishlaring yaxshimi?")
+print(f"Mahmud bugun maktabga bordingmi?")
+print(f"Kecha tushgan rasmlarni tashlavor {friends[2]}")
