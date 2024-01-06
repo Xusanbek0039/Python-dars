2c6c07832db1b1c8f91afc2e3cac6d0c66f699b7
Xusanbek Suyunov <109731657+Xusanbek0071@users.noreply.github.com>
2024-01-06 12:41:38 +0500

    Create 79.py

---
+++ Dars_28/Javoblar/79.py
@@ -0,0 +1,14 @@
+"""NumPy kutubxonasidan foydalangan holda kamida 10 qatordan iborat kod yozing hamda sharxlab chiqing.
+Darslikda foydalanilgan kodlardan foydalanmang."""
+from numpy import random    # numpy qilib yozib oldim
+r=random.randint(17)    #  r deb o'zgaruvchi oldik randint qilib 17 qildim
+
+p=random.rand(65)           # p o'zgaruvchi oldik  rand qilib 65 qildim
+
+y=random.randint(8)         # y o'zgaruvchi oldik   randint qilib 8 qildim
+
+g=random.rand(6)              # g o'zgaruvchi oldik    rand  qilib 6 qildim 
+
+x=random.randint(498)         # x o'zgaruvchi oldik    randint qilib 498 qildim
+
+print(x,r,p,y,g)     # print javobni yukladim
