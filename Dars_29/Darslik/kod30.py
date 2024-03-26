322f917fd338982deedd0fcae7c6b4e7f15ad5ad
Xusanbek Suyunov <109731657+Xusanbek0071@users.noreply.github.com>
2024-03-26 11:38:27 +0300

    a

---
+++ Dars_29/Darslik/kod30.py
@@ -0,0 +1,173 @@
+"""Kod_30 Kutubxonalar mavzusi taqdimotlar darsligi davomiyligi"""
+
+
+
+# # Kod_1
+# import numpy as np
+
+# def myadd(x, y):
+#   return x+y
+
+# myadd = np.frompyfunc(myadd, 2, 1)
+
+# print(myadd([1, 2, 3, 4], [5, 6, 7, 8]))
+
+
+
+
+
+
+
+# # Kod_2
+# import numpy as np
+
+# print(type(np.add))
+
+
+
+
+
+# # Kod_3
+# import numpy as np
+
+# print(type(np.concatenate))
+
+
+
+
+
+
+
+# # Kod_4
+# import numpy as np
+
+# print(type(np.abcdefg))
+
+
+
+
+
+
+# #  Kod_5
+# import numpy as np
+
+# if type(np.add) == np.ufunc:
+#   print('add is ufunc')
+# else:
+#   print('add is not ufunc')
+
+
+
+
+
+
+
+# Kod_6
+# import numpy as np
+
+# arr1 = np.array([10, 11, 12, 13, 14,25])
+# arr2 = np.array([20, 21, 22, 23, 24, 25])
+
+# newarr = np.add(arr1, arr2)
+
+# print(newarr)
+
+
+
+
+
+
+
+# # Kod_7
+# import numpy as np
+
+# arr1 = np.array([10, 20, 30, 40, 50, 60])
+# arr2 = np.array([20, 21, 22, 23, 24, 25])
+
+# newarr = np.subtract(arr1, arr2)
+
+
+# print(newarr)
+
+
+
+
+
+# # Kod_8
+# import numpy as np
+
+# arr1 = np.array([10, 20, 30, 40, 50, 60])
+# arr2 = np.array([20, 21, 22, 23, 24, 25])
+
+# newarr = np.multiply(arr1, arr2)
+
+# print(newarr)
+
+
+
+
+
+
+
+# # Kod_9
+# import numpy as np
+
+# arr1 = np.array([10, 20, 30, 40, 50, 60])
+# arr2 = np.array([3, 5, 10, 8, 2, 33])
+
+# newarr = np.divide(arr1, arr2)
+
+# print(newarr)
+
+
+
+
+
+
+# kod_10
+
+# import numpy as np
+
+# arr1 = np.array([10])
+# arr2 = np.array([3])
+
+# newarr = np.power(arr1, arr2)
+
+# print(newarr)
+
+
+
+
+
+
+# # Kod_11
+# import numpy as np
+
+# arr1 = np.array([10])
+# arr2 = np.array([3])
+
+# newarr = np.mod(arr1, arr2)
+
+# print(newarr)
+
+
+
+
+
+
+
+# # Kod_12
+# import numpy as np
+
+# arr1 = np.array([10, 20, 30, 40, 50, 60])
+# arr2 = np.array([3, 7, 9, 8, 2, 33])
+
+# newarr = np.remainder(arr1, arr2)
+
+# print(newarr)
+
+
+
+
+
+
