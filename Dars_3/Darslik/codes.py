3a819f94adfd7bc6cf262c9b5a566886f7f65db1
Xusanbek Suyunov <109731657+Xusanbek0039@users.noreply.github.com>
2025-02-11 20:14:12 +0500

    Update codes.py

--- Dars_3/Darslik/codes.py
+++ Dars_3/Darslik/codes.py
@@ -9,11 +9,34 @@
 
 
 # 2-code 
-meva = "        banan           "
-print("Men "+meva+" ni yaxshi ko'raman!")
+# meva = "        banan           "
+# print("Men "+meva+" ni yaxshi ko'raman!")
 
 
 
 
 
 
+# 3-code 
+# meva = "        banan           "
+# print("Men "+meva.rstrip()+" ni yaxshi ko'raman!")
+
+
+
+
+
+
+
+# # 4-code 
+# meva = "        banan           "
+# print("Men "+meva.lstrip()+" ni yaxshi ko'raman!")
+
+
+
+
+
+
+
+# 5-code 
+# meva = "        banan           "
+# print("Men "+meva.strip()+" ni yaxshi ko'raman!")
