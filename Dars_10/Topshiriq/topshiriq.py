4250c59f3afa30347d877f5fdf3dc434a3f248e0
Xusanbek Suyunov <109731657+Xusanbek0039@users.noreply.github.com>
2025-02-14 18:06:35 +0500

    Update

--- Dars_10/Topshiriq/topshiriq.py
+++ Dars_10/Topshiriq/topshiriq.py
@@ -23,32 +23,18 @@ Aks holda → "F"
 
 
 
+# score = int(input("Baxoni kiriting: "))
 
-
-
-
-
-
-
-
-
-
-
-
-
-# scores = int(input("Baxoni kiriting: "))
-
-# for score in scores:
-#     if score >= 90:
-#         print(score, "→ A")
-#     elif score >= 80:
-#         print(score, "→ B")
-#     elif score >= 70:
-#         print(score, "→ C")
-#     elif score >= 60:
-#         print(score, "→ D")
-#     else:
-#         print(score, "→ F")
+# if score >= 90:
+#     print(score, "→ A")
+# elif score >= 80:
+#     print(score, "→ B")
+# elif score >= 70:
+#     print(score, "→ C")
+# elif score >= 60:
+#     print(score, "→ D")
+# else:
+#     print(score, "→ F")
 
 
 
@@ -56,36 +42,31 @@ Aks holda → "F"
 
 
 """
-2-masala
-1 dan N gacha bo‘lgan sonlar orasidan:
-
-Agar son 3 ga bo‘linsa → "Fizz"
-Agar son 5 ga bo‘linsa → "Buzz"
-Agar son 3 hamda 5 ga bo‘linsa → "FizzBuzz"
-Aks holda sonning o‘zi chiqarilsin.
+    2-masala
+    Qabul qilingan sonni 
+    Agar son 3 ga bo‘linsa → "Fizz"
+    Agar son 5 ga bo‘linsa → "Buzz"
+    Agar son 3 hamda 5 ga bo‘linsa → "FizzBuzz"
+    Aks holda sonning o‘zi chiqarilsin.
 """
 
 
-# n = 20  
-
-# for i in range(1, n + 1):  
-#     if i % 3 == 0 and i % 5 == 0:  
-#         print("FizzBuzz")  
-#     elif i % 3 == 0:  
-#         print("Fizz")  
-#     elif i % 5 == 0:  
-#         print("Buzz")  
-#     else:  
-#         print(i)  
 
 
 
 
 
 
+# i = int(input("Son kiriting: "))
 
-
-
+# if i % 3 == 0 and i % 5 == 0:  
+#     print("FizzBuzz")  
+# elif i % 3 == 0:  
+#     print(f"Fizz {i}")  
+# elif i % 5 == 0:  
+#     print(f"Buzz {i}")  
+# else:  
+#     print(i)  
 
 
 """
@@ -96,13 +77,21 @@ sanaydigan dastur yozing.
 
 
 
+
+
+
+
+
+
+
+
 matin = "Python dasturlash tili"  
 unlilar = "aeiouAEIOU"  
 son = 0  
 
 for raqam in matin:  
     if raqam in unlilar:  
-        son += 1  
+        son = 1 + son
 
 print("Unli harflar soni:", son)  
 
@@ -112,25 +101,32 @@ print("Unli harflar soni:", son)
 
 
 
-
 """
 4-masala
-Berilgan ro‘yxatdagi sonlarning o‘rtacha qiymatini hisoblovchi dastur yozing.
+Berilgan ro‘yxatdagi sonlarning o‘rtacha 
+qiymatini hisoblovchi dastur yozing.
 """
+sonlar = []
+for i in range(5):
+    son = int(input(f"{i+1}-sonni kiriting: "))
+    sonlar.append(son)
+
+bolish = len(sonlar)
+summ = 0
+for raqam in sonlar:
+    summ += raqam
+
+
+print(f"{sonlar} yigindisi {summ} o'rtacha qiymati {summ//raqam}")
 
 
 
 
+numbers = [12, 45, 78, 34, 56, 89, 23]  
+sum_numbers = 0  
 
+for num in numbers:  
+    sum_numbers += num  
 
-
-
-
-# numbers = [12, 45, 78, 34, 56, 89, 23]  
-# sum_numbers = 0  
-
-# for num in numbers:  
-#     sum_numbers += num  
-
-# average = sum_numbers / len(numbers)  
-# print("O‘rtacha qiymat:", average)  
+average = sum_numbers / len(numbers)  
+print("O‘rtacha qiymat:", average)  
---
+++ Dars_4/Darslik/codes.py
@@ -0,0 +1,22 @@
+# a = 30
+# b = -20
+# print(a+b)
+
+
+
+
+# a = 10
+# b = 20.7
+# c = "Python"
+# d = True
+
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+
+
+
+# a = int(input("Yoshingizni kiriting: "))
+# c = 2025-a
+# print(f"Sizning tug'ilgan yilingiz: {c}-yil")
