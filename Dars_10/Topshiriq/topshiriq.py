61c0723533aeb531f0d1635511bb28ea4888d6e4
Xusanbek Suyunov <109731657+Xusanbek0039@users.noreply.github.com>
2025-02-14 09:15:29 +0500

    Create topshiriq.py

---
+++ Dars_10/Topshiriq/topshiriq.py
@@ -0,0 +1,44 @@
+"""
+Foydalanuvchi baho kiritadi. Dastur ushbu bahoga qarab 
+quyidagi natijani chiqaradi:
+
+90 dan katta bo‘lsa → "A"
+80 dan katta bo‘lsa → "B"
+70 dan katta bo‘lsa → "C"
+60 dan katta bo‘lsa → "D"
+Aks holda → "F"
+"""
+
+
+
+
+
+
+# scores = int(input("Baxoni kiriting: "))
+
+# for score in scores:
+#     if score >= 90:
+#         print(score, "→ A")
+#     elif score >= 80:
+#         print(score, "→ B")
+#     elif score >= 70:
+#         print(score, "→ C")
+#     elif score >= 60:
+#         print(score, "→ D")
+#     else:
+#         print(score, "→ F")
+
+
+
+
+
+
+"""
+2-masala
+1 dan N gacha bo‘lgan sonlar orasidan:
+
+Agar son 3 ga bo‘linsa → "Fizz"
+Agar son 5 ga bo‘linsa → "Buzz"
+Agar son 3 hamda 5 ga bo‘linsa → "FizzBuzz"
+Aks holda sonning o‘zi chiqarilsin.
+"""
