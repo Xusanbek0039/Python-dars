a7ce67431f343b38387cea002ec3381761c07539
Xusanbek Suyunov <109731657+Xusanbek0039@users.noreply.github.com>
2025-02-14 09:18:37 +0500

    Update topshiriq.py

--- Dars_10/Topshiriq/topshiriq.py
+++ Dars_10/Topshiriq/topshiriq.py
@@ -1,4 +1,5 @@
 """
+1-masala
 Foydalanuvchi baho kiritadi. Dastur ushbu bahoga qarab 
 quyidagi natijani chiqaradi:
 
@@ -42,3 +43,44 @@ Agar son 5 ga bo‘linsa → "Buzz"
 Agar son 3 hamda 5 ga bo‘linsa → "FizzBuzz"
 Aks holda sonning o‘zi chiqarilsin.
 """
+
+
+# n = 20  
+
+# for i in range(1, n + 1):  
+#     if i % 3 == 0 and i % 5 == 0:  
+#         print("FizzBuzz")  
+#     elif i % 3 == 0:  
+#         print("Fizz")  
+#     elif i % 5 == 0:  
+#         print("Buzz")  
+#     else:  
+#         print(i)  
+
+
+
+
+
+
+
+
+
+
+
+"""
+3-masala
+Foydalanuvchi kiritgan matndagi unli harflarni 
+sanaydigan dastur yozing.
+"""
+
+
+
+text = "Python dasturlash tili"  
+vowels = "aeiouAEIOU"  
+count = 0  
+
+for char in text:  
+    if char in vowels:  
+        count += 1  
+
+print("Unli harflar soni:", count)  
