8331c7e4e3cd5a15753e78b411dbcabb124d196d
Xusanbek Suyunov <109731657+Xusanbek0039@users.noreply.github.com>
2025-02-16 23:49:24 +0500

    Update test.py

--- Dars_10/Darslik/test.py
+++ Dars_10/Darslik/test.py
@@ -1,5 +1,16 @@
-n = int(input("Son kiriting: "))
-if n % 2 == 0:
-    print("Bu son juft.")
-else:
-    print("Bu son toq.")
+# 1-masala
+# n = int(input("Son kiriting: "))
+# if n % 2 == 0:
+#     print("Bu son juft.")
+# else:
+#     print("Bu son toq.")
+
+
+
+
+
+
+
+# 2-masala
+# n = int(input("Son kiriting: "))
+# print(f"Kvadrat: {n**2}, Kub: {n**3}")
