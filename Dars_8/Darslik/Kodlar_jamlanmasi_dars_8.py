64e0597ccce45040c3c1461a7322d1492a7e6b2d
Xusanbek Suyunov <109731657+Xusanbek0039@users.noreply.github.com>
2025-02-17 01:25:38 +0500

    Update Kodlar_jamlanmasi_dars_8.py

--- Dars_8/Darslik/Kodlar_jamlanmasi_dars_8.py
+++ Dars_8/Darslik/Kodlar_jamlanmasi_dars_8.py
@@ -13,13 +13,16 @@
 
 
 
+
+
+
 # # Kod_2
 # avtolar = ['audi','bmw','volvo','kia','hyundai']
 # for avto in avtolar: # avtolar ichidadi har bir avto uchun ...
-#    if avto == 'bmw':  # ... agar avto bmw ga teng bo'lsa ...
-#        print(avto.upper()) # avto nomini hamma harflarini katta bilan yoz.
-#    else: # aks holda ...
-#        print(avto.title()) # avto nomini faqat birinchi harfini katta bilan yoz.
+#     if avto == 'bmw':  # ... agar avto bmw ga teng bo'lsa ...
+#         print(avto.upper()) # avto nomini hamma harflarini katta bilan yoz.
+#     else: # aks holda ...
+#         print(avto.title()) # avto nomini faqat birinchi harfini katta bilan yoz.
 
 
 
@@ -32,8 +35,8 @@
 
 
 # # Kod_3
-# ism = input('Ismingiz nima?\n>>>') # Foydalanuvchi ismini so'raymiz
-# if ism.lower() != 'ali': # Agar ism Aliga teng bo'lmasa ...
+# ism = input('Ismingiz nima?\t>>>') # Foydalanuvchi ismini so'raymiz
+# if ism.lower() == 'ali': # Agar ism Aliga teng bo'lmasa ...
 #    print(f"Uzr, {ism.title()} biz Alini kutayapmiz.") # quyidagi xabar chiqadi
 # else:
 #     print("Salom, Ali")
@@ -44,10 +47,22 @@
 
 
 
+
+
+
+
 # # Kod_4
-# javob = float(input("12x6 nechiga teng?>>>"))
-# if javob!=72:
+# javob = float(input("12x6 nechiga teng?>>>  "))
+# if javob != 72:
 #    print("Javob xato!")
+# else:
+#    print("Barakalla! Javobingiz to'g'ri.")
+
+
+
+
+
+
 
 
 
@@ -57,8 +72,8 @@
 
 
 # # Kod_5
-# yosh = int(input("Yoshingiz nechida?>>>"))
-# if yosh>=18: # yosh 18 dan katta yoki teng bo'lsa
+# yosh = int(input("Yoshingiz nechida?>>>  "))
+# if yosh >= 18: # yosh 18 dan katta yoki teng bo'lsa
 #    print('Xush kelibsiz!')
 # else: # ask holda
 #    print('Kirish mumkin emas!')
@@ -73,10 +88,16 @@
 
 
 
+
+
 # # Kod_6
-# login = input("Yangi login tanlang:")
-# if len(login)<=5: # login uzunligini tekshiramiz
+# login = input("Yangi login tanlang: ")
+# if len(login) <= 5: # login uzunligini tekshiramiz
 #    print("Login 5 harfdan ko'proq bo'lishi shart!")
+# else:
+#    print(f"Siz kiritgan {login} login sifatida tizimga saqlandi!")
+
+
 
 
 
@@ -90,8 +111,8 @@
 
 
 # # Kod_7
-# yil = int(input("Tug'ilgan yilingizni kiriting:"))
-# if 2023-yil<18: # foydalanuvchining yoshini hisoblaymiz
+# yil = int(input("Tug'ilgan yilingizni kiriting: "))
+# if 2025-yil < 18: # foydalanuvchining yoshini hisoblaymiz
 #    print(f"Yoshingiz {2023-yil}da ekan.")
 #    print("Kirish mumkin emas!")
 # else:
@@ -104,6 +125,12 @@
 
 
 
+
+
+
 # # Kod_8
-# yosh = int(input("Yoshingiz nechida?>>>"))
-# if yosh>65: print("Siz COVID-19 risk guruhida ekansiz"
+# yosh = int(input("Yoshingiz nechida?>>> "))
+# if yosh > 65: 
+#     print("Siz COVID-19 risk guruhida ekansiz")
+# else:
+#     print("SIZ MUTLAQO SOG'LOMSIZ!")
