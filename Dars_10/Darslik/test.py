8540279def525b8e2598b7b0689633df2e72fabc
Xusanbek Suyunov <109731657+Xusanbek0039@users.noreply.github.com>
2025-02-16 23:55:34 +0500

    Update test.py

--- Dars_10/Darslik/test.py
+++ Dars_10/Darslik/test.py
@@ -14,3 +14,37 @@
 # 2-masala
 # n = int(input("Son kiriting: "))
 # print(f"Kvadrat: {n**2}, Kub: {n**3}")
+
+
+
+
+# 3-masala
+# a = int(input("Birinchi son: "))
+# b = int(input("Ikkinchi son: "))
+# print(f"Kattasi: {max(a, b)}")
+
+
+
+
+
+
+# 4-masala
+# n = int(input("N sonini kiriting: "))
+# yigindi = sum(range(1, n+1))
+# print(f"Yig‘indi: {yigindi}")
+
+
+
+
+# 5-masala
+# n = int(input("Nechta Fibonachchi soni kerak? "))
+# fib = [0, 1]
+# for i in range(2, n):
+#     fib.append(fib[-1] + fib[-2])
+# print(fib[:n])
+
+
+
+
+
+# 6-masala
