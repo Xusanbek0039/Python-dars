e78765dcc29161a50542a1adc60ff187b7029595
Xusanbek Suyunov <109731657+Xusanbek0039@users.noreply.github.com>
2025-02-19 21:53:04 +0500

    Create code.py

---
+++ Dars_10/Darslik/code.py
@@ -0,0 +1,67 @@
+# 1-masala
+# ball = int(input("Ballingizni kiriting (0-100): "))
+# if 0 <= ball <=54:
+#     print("Sizning ballingiz: 2")
+# elif 55 <= ball <= 69:
+#     print("Sizning ballingiz: 3")
+# elif 70 <= ball <= 84:
+#     print("Sizning ballingiz: 4")
+# elif 85 <= ball <= 100:
+#     print("Sizning ballingiz: 5")
+# else:
+#     print(f"{ball}-notog'ri qiymat!")
+
+
+
+
+
+
+
+# 2-masala
+# harorat = int(input("Havo haroratini kiriting:\t"))
+# if harorat < 0:
+#     print("Havo juda sovuq! Issiq kiyining.")
+# elif 0 <= harorat <=15:
+#     print("Salqin havo, kurtka kiying!")
+# elif 16 <= harorat <= 25:
+#     print("Ob-havo yaxshi yengil kiyinishingiz mumkin!")
+# else:
+#     print("Juda issiq! Ko'proq suv iching.")
+
+
+
+
+
+# 3-masala
+# masofa = int(input("Masofani kiriting (km): "))
+# if masofa <= 5:
+#     print("Taxi narxi: 5 000 so'm")
+# elif 6 <= masofa <= 15:
+#     print("Taxi narxi: 10 000 so'm")
+# elif 16 <= masofa <= 30:
+#     print("Taxi narxi: 20 000 so'm")
+# else:
+#     print("Taxi narxi: 30 000 so'm")
+
+
+
+
+# 4-masala
+# yosh = int(input("Yoshingizni kiriting: "))
+# boy = int(input("Bo'yingizni kiriting: "))
+
+# if yosh <= 10:
+#     print("Suzish yoki gimnastika bilan shug'ullaning!")
+# elif 11 <= yosh <= 18 and boy >170:
+#     print("Basketbol siz uchun ma'qul!")
+# elif 11 <= yosh <18 and boy > 160:
+#     print("Siz uchun futbool ma'qul!")
+# else:
+#     print("Jismoniy mashqlar!")
+
+
+
+
+
+
+# 5-masala
