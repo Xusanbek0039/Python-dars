ed833dbabb0ca59fd206b42f756d4ea4777ae750
Xusanbek Suyunov <109731657+Xusanbek0039@users.noreply.github.com>
2025-02-26 12:05:56 +0500

    Update kodlar_jamlanmasi.py

--- Dars_12/Darslik/kodlar_jamlanmasi.py
+++ Dars_12/Darslik/kodlar_jamlanmasi.py
@@ -151,6 +151,7 @@
 #     'husan':['python','php'],
 #     'maryam':['c++','c#']
 #     }
+
 # for ism, tillar in dasturchilar.items():
 #     print(f"\n{ism.title()} quyidagi dasturlash tillarini biladi:")
 #     for til in tillar:
