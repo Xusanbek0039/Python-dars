462a2e5d5d618d1265a96b033edfc033097dfa6b
Xusanbek Suyunov <109731657+Xusanbek0039@users.noreply.github.com>
2025-03-05 00:18:01 +0500

    Update Dars_16_kodlar_jamlanmasi.py

--- Dars_15/Darslik/Dars_16_kodlar_jamlanmasi.py
+++ Dars_15/Darslik/Dars_16_kodlar_jamlanmasi.py
@@ -1,22 +1,37 @@
 # kod_1
+
 # ismlar = []
 
 # print("Yaqin do'stlaringiz ro'yxatini tuzamiz.")
 # n=1 # ismlarni sanash uchun o'zgaruvchi
 # while True:
-#     savol = f"{n}-do'stingiz ismini kiriting:"
+#     savol = f"{n}-do'stingiz ismini kiriting: "
 #     ism = input(savol)
 #     ismlar.append(ism)
-#     javob = input("Yana ism qo'shasizmi? (ha/yo'q)")
+#     javob = input("Yana ism qo'shasizmi? (ha/yo'q) ")
+#     javob = javob.lower()
+#     javob = javob.strip()
 #     if javob =='ha':
-#         n+=1
+#         n=1+n
 #         continue
-#     else:
+#     elif javob == "yo'q":
 #         break
+#     else:
+#         print("Iltimos to'g'ri matindan foydalaning ha/yo'q: ")
 
-# print("Do'stlaringiz ro'yxati:")
+
+# print("Do'stlaringiz ro'yxati: ")
 # for ism in ismlar:
 #     print(ism.title())
+
+
+
+
+
+
+
+
+
 
 
 
@@ -39,12 +54,24 @@
 #     yosh = input(f"{ism.title()}ning yoshini kiriting: ")
 #     dostlar[ism] = int(yosh) # ism kalit, yosh qiymat
     
-#     javob = input("Yana ma'lumot qo'shasizmi? (ha/yo'q)")
+#     javob = input("Yana ma'lumot qo'shasizmi? (ha/yo'q)\t")
+#     javob = javob.lower()
+#     javob = javob.strip()
 #     if javob == "yo'q":
 #         ishora = False
+#     elif javob == "ha":
+#         continue
+
 
 # for ism, yosh in dostlar.items():
-#     print(f"{ism.title()} {yosh} yoshda")
+#     print(f"\n{ism.title()} {yosh} yoshda!")
+
+
+
+
+
+
+
 
 
 
@@ -90,3 +117,4 @@
 #     baho = input(f"{talaba.title()}ning bahosini kiriting: ")
 #     print(f"{talaba.title()} baholandi")
 #     baholangan_talabalar[talaba] = baho
+# print(baholangan_talabalar)
