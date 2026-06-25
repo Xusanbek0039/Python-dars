16ea5d70dd4b8ed64509616920e510582fa74793
Husanbek Suyunov <109731657+Xusanbek0039@users.noreply.github.com>
2026-06-25 14:17:25 +0500

    Update _content_11_20.py

---
+++ _content_11_20.py
@@ -0,0 +1,11 @@
+malumot =['burxon', 'Meliyev', 1,2,3,True,True]
+intlar = 0
+strlar = 0
+bolang = 0
+
+for i in malumot:
+    if type(i) == int:
+        intlar =+1
+    elif type(i) == str
+        strlar 
+
